761c15f143e3dbf799c8c565eb2269ba400a4627
MidoriyaHero <buitrungtin0105@gmail.com>
2024-08-19 23:05:32 +0700

    add MovieList table, add movie + add list function in views.py

--- core/models.py
+++ core/models.py
@@ -25,3 +25,10 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
+    
+class MovieList(models.Model):
+    owner_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete= models.CASCADE,
+    )
+    movie = models.ForeignKey(Movie, on_delete= models.CASCADE)
--- core/views.py
+++ core/views.py
@@ -1,20 +1,40 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from .models import Movie
+from .models import Movie, MovieList
 from django.contrib.auth.decorators import login_required
-
+from django.http import JsonResponse
+import re
+from django.shortcuts import get_object_or_404
 # Create your views here.
 @login_required(login_url='login')
 def index(request):
     movies = Movie.objects.all()
     context = {
-        'movie': movies,
+        'movies': movies,
     }
     return render(request, 'index.html',context)
 
 def add_to_list(request):
-    pass
+    if request.method == 'POST':
+        movie_url_id = request.POST.get('movie_id')
+        
+        uuid_pattern =  r'[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}'
+        match = re.search(uuid_pattern, movie_url_id)
+        movie_id = match.group() if match else None
+        
+        movie = get_object_or_404(Movie,uu_id = movie_id)
+        movie_list ,created = MovieList.objects.get_or_create(owner_user = request.user, movie = movie) 
+
+        if created:
+            response_data = {'status': 'Success', 'message' : 'Movie created successfully!'}
+        else:
+            response_data = {'status': 'info', 'message' : 'Movie already added!'}
+
+        return JsonResponse(response_data)
+    else:
+        response_data = {'status': 'error', 'message' : 'Something went wrong!'}
+        return JsonResponse(response_data, status = 400)
 
 def userlogin(request):
     if request.method == 'POST':
@@ -65,13 +85,27 @@ def movie(request,pk):
     context = {
         'movie_details': movie_details
     }
-    return render(request,'movie',context)
+    return render(request,'movie.html',context)
 
 def genre(request):
     pass
 
+@login_required(login_url='login')
 def my_list(request):
-    pass
+    movie_list = MovieList.objects.filter(owner_user = request.user)
+    user_list = []
+    for movie in movie_list:
+        user_list.append(movie.movie)
+    context = {
+        "movies": user_list
+    }
+    return render(request,'my_list.html', context)
 
+@login_required(login_url='login')
 def search(request):
     pass
+
+@login_required(login_url='login')
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
